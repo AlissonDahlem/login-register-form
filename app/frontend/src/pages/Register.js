37d387ee26dc074873e3444d13c3ec0c6ab5634f
Alisson Dahlem <alissongod95@hotmail.com>
2022-09-03 11:15:54 -0300

    feat: Foram trazidas ao front-end as mensagens de validação caso retorne algum erro do backendf

--- app/frontend/src/pages/Register.js
+++ app/frontend/src/pages/Register.js
@@ -1,29 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import './register.css'
 
 export default function Register(props) {
   const [userFirstName, setUserFirstName] = useState();
   const [userLastName, setUserLastName] = useState();
   const [userEmail, setUserEmail] = useState();
   const [userPassword, setUserPassword] = useState();
+  const [userConfirmPassword, setUserConfirmPassword] = useState();
   const [isActivedRegisterButton, setIsActivedRegisterButton] = useState(true);
+  const [backendRegisterReturned, setBackendRegisterReturned] = useState();
+  
 
   useEffect(() => {
-    checkInputs(userFirstName, userLastName, userEmail, userPassword)
-  }, [userFirstName, userLastName, userEmail, userPassword])
+    function checkInputs(userFirstName, userLastName, userEmail, userPassword) {
+      const checkFirstName = userFirstName === undefined ? false: userFirstName.length > 3;
+      const checkLastName = userLastName === undefined ? false: userLastName.length > 3;
+      const checkUserEmail = userEmail === undefined ? false : userEmail.length > 8
+      const checkPassword = userPassword === undefined ? false : userPassword.length > 4
 
-  function checkInputs(userFirstName, userLastName, userEmail, userPassword) {
-    const checkFirstName = userFirstName === undefined ? false: userFirstName.length > 3;
-    const checkLastName = userLastName === undefined ? false: userLastName.length > 3;
-    const checkUserEmail = userEmail === undefined ? false : userEmail.length > 8
-    const checkPassword = userPassword === undefined ? false : userPassword.length > 6
-    if (checkFirstName && checkLastName && checkUserEmail && checkPassword) {
-      setIsActivedRegisterButton(false)
-    }
-  };
+      if (checkFirstName && checkLastName && checkUserEmail && checkPassword) {
+        setIsActivedRegisterButton(false)
+      } else {
+        setIsActivedRegisterButton(true)
+      }
+    };
+    checkInputs(userFirstName, userLastName, userEmail, userPassword)
+  }, [userFirstName, userLastName, userEmail, userPassword, userConfirmPassword,])
 
   async function registerButton() {
-
+    const confirmPassword = userPassword === userConfirmPassword
+    const { history } = props
+    if(!confirmPassword) {
+      return setBackendRegisterReturned('SENHA DIFERENTE PORRA')
+    }
     const request = await fetch('http://localhost:3001/user', {
       method: 'POST',
       headers: {
@@ -35,11 +43,16 @@ export default function Register(props) {
           email: userEmail,
           password: userPassword,
       }),
-      // mode: 'no-cors',
       cache: 'default',
     })
     const data = await request.json()
-    
+    if (data.message) {
+      return setBackendRegisterReturned(data.message);
+    } if (data.ok) {
+      history.push('/')
+    } else {
+      return setBackendRegisterReturned('Server problem')
+    }
   }
 
   function handleClick() {
@@ -80,7 +93,7 @@ export default function Register(props) {
             onChange={({ target }) => setUserEmail(target.value)}
           />
             
-          <hr className='linha'/>
+          <hr className='linha' style={{backgroundColor: 'red'}}/>
           <p>Password</p>
           <input
             placeholder='Type your password'
@@ -89,6 +102,22 @@ export default function Register(props) {
             onChange={({ target }) => setUserPassword(target.value)}
           />
           <hr className='linha'/>
+
+          <p>Confirm your password</p>
+          <input
+            placeholder='Confirm your password'
+            className='input'
+            type='password'
+            onChange={({ target }) => setUserConfirmPassword(target.value)}
+          />
+          <hr className='linha'/>
+
+          <div style={{ height: '5px'}}>
+            <p style={{color: 'red' }}>
+              {backendRegisterReturned}
+            </p>
+          </div>
+
         </form>
 
         <div className='buttons'>
