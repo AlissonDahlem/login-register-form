7b20e49a23fd6069798769be6f666d51020d2b49
Alisson Dahlem <alissongod95@hotmail.com>
2022-09-03 00:54:50 -0300

    feat: adiciona tela para registro de usuario

--- app/frontend/src/pages/Login.js
+++ app/frontend/src/pages/Login.js
@@ -4,32 +4,31 @@ import React from 'react';
 import './login.css'
 
 function Login(props) {
-  // function handleClick() {
-  //   const { history } = props;
-  //   history.push('/register')
-  // }
+  function handleClick() {
+    const { history } = props;
+    history.push('/register')
+  }
   return (
     <div className="loginPage">
       <div className="loginModal">
         <div>
-          <h1 id='loginText'>Login</h1>
+          <h1 className='loginText'>Login</h1>
         </div>
-        <form id='loginForm'>
+        <form className='loginForm'>
           <p>E-mail</p>
           <input placeholder='Type your e-mail' className='input'/>
           <hr className='linha'/>
           <p>Password</p>
-          <input placeholder='Type your password' className='input'/>
+          <input placeholder='Type your password' className='input' type='password'/>
           <hr className='linha'/>
           <h2>Forgot password?</h2>
         </form>
         <div className='buttons'>
         </div>
-          <button id='loginButton' type='button'>LOGIN</button>
-          <p id='signUpButton'>SIGN UP</p>
+          <button className='loginButton' type='button'>LOGIN</button>
+          <p className='signUpButton' onClick={() => handleClick()}>SIGN UP</p>
       </div>
     </div>
-
   )
 } 
 
--- app/frontend/src/pages/Register.js
+++ app/frontend/src/pages/Register.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import './register.css'
 
-export default function Register() {
+export default function Register(props) {
+
+function handleClick() {
+  const { history } = props;
+  history.push('/')
+}
   return(
-    <>
-      <h1>register</h1>
-    </>
+    <div className="loginPage">
+      <div className="loginModal">
+        <div>
+          <h1 className='loginText'>Register</h1>
+        </div>
+        <form className='loginForm'>
+          <p>First name</p>
+          <input placeholder='Type your first name' className='input'/>
+          <hr className='linha'/>
+          <p>Last name</p>
+          <input placeholder='Type your last name' className='input'/>
+          <hr className='linha'/>
+          <p>E-mail</p>
+          <input placeholder='Type your e-mail' className='input'/>
+          <hr className='linha'/>
+          <p>Password</p>
+          <input placeholder='Type your password' className='input' type='password'/>
+          <hr className='linha'/>
+        </form>
+        <div className='buttons'>
+        </div>
+          <button className='registerButton' type='button'>Register</button>
+          <p
+          className='AlreadyRegisteredButton'
+          onClick={() => handleClick()}
+          >
+            Already registered?
+          </p>
+      </div>
+    </div>
   )
 }
